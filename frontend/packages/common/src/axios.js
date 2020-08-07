2d55c7a275b01b217aedd8baead5814d0f162920
Lootfi <lotfiholmes@hotmail.com>
2020-08-07 13:14:57 +0100

    https

--- frontend/packages/common/src/axios.js
+++ frontend/packages/common/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `http://www.contactmajor.com/api/`
+  baseURL: `https://www.contactmajor.com/api/`
 });
 
 export default instance;
