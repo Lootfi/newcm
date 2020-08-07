792a4df8c93954d92517c74721308cc9f1e41e8f
Lootfi <lotfiholmes@hotmail.com>
2020-08-07 15:17:28 +0100

    change api endpoint

--- frontend/packages/common/src/axios.js
+++ frontend/packages/common/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `https://api.contactmajor.com/api/`
+  baseURL: `https://dashboard.contactmajor.com/api/`
 });
 
 export default instance;
