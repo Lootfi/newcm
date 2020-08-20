3478c339266987bf12ae4a7b6e6df03a10a6482a
Lootfi <lotfiholmes@hotmail.com>
2020-08-20 16:35:07 +0100

    styling 404

--- frontend/packages/landing-gatsby/src/pages/404.js
+++ frontend/packages/landing-gatsby/src/pages/404.js
@@ -2,13 +2,66 @@ import React from 'react';
 import SEO from '../components/seo';
 import ErrorSec from '../containers/Error';
 import { ResetCSS } from '../../../common/src/assets/css/style';
+import Template from './template-app';
+import Container from 'common/src/components/UI/Container';
+import { NotFoundWrapper } from '../styles/notfound.style';
+import Footer from '../containers/SaasClassic/Footer';
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <ResetCSS />
-    <ErrorSec></ErrorSec>
-  </>
-);
+import Icon from 'react-icons-kit';
+import { home } from 'react-icons-kit/fa/home';
+import { checkSquareO } from 'react-icons-kit/fa/checkSquareO';
+import { Link } from 'gatsby';
+const NotFoundPage = () => {
+  const smtWrong = React.useRef();
+  React.useEffect(() => {
+    var i = 0;
+    var txt = 'Something Went Wrong';
+    var speed = 100;
+
+    function typeWriter() {
+      if (i < txt.length) {
+        smtWrong.current.innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      } else {
+        setTimeout(() => {
+          smtWrong.current.innerHTML = '';
+          i = 0;
+          typeWriter();
+        }, 800);
+      }
+    }
+    typeWriter();
+  }, []);
+  return (
+    <Template>
+      <SEO title="404: Not found" />
+      <NotFoundWrapper>
+        <Container>
+          <div className="content-table-cell">
+            <div className="spanParent">
+              <span className="animation-text">404</span>
+              <span className="error">Erreur</span>
+            </div>
+            <h3 className="notFound">Page Non Trouvée</h3>
+            <h4 ref={smtWrong} className="smtWrong"></h4>
+          </div>
+          <br />
+          <br />
+          <br />
+          <div className="bottomRow">
+            <Link to="/" style={{ color: 'white' }}>
+              <Icon icon={home} size={20} /> Acceuil
+            </Link>
+            <span>
+              <Icon icon={checkSquareO} size={20} /> Report Us
+            </span>
+          </div>
+        </Container>
+      </NotFoundWrapper>
+      <Footer />
+    </Template>
+  );
+};
 
 export default NotFoundPage;
---
+++ frontend/packages/landing-gatsby/src/styles/notfound.style.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import ErrorImage from 'common/src/assets/image/error.png';
+export const NotFoundWrapper = styled.div`
+  padding-top: 80px;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  @keyframes aitf {
+    0% {
+      background-position: 0% 50%;
+    }
+    100% {
+      background-position: 100% 50%;
+    }
+  }
+  .animation-text {
+    font-size: 15rem;
+    margin: 0;
+    background: url(${ErrorImage}) repeat-y;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: aitf 80s linear infinite;
+    transform: translate3d(0, 0, 0);
+    backface-visibility: hidden;
+    font-weight: 700;
+    text-transform: uppercase;
+  }
+
+  .notFound {
+    color: #00acc1;
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  .bottomRow {
+    & > * {
+      margin: 5px;
+    }
+  }
+
+  @media (max-width: 550px) {
+    .spanParent {
+      display: flex;
+      flex-direction: column;
+    }
+    .animation-text {
+      line-height: 1.5;
+      font-size: 8em;
+    }
+  }
+  .error {
+    font-size: 2em;
+    font-weight: 700;
+    text-transform: uppercase;
+    position: relative;
+    top: -38px;
+  }
+`;
